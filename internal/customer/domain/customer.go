5ddd1e06c59e57e64418f1fc433aa2fb2aa7aa48
Sadegh <Sadeghkhoshbayan1@gmail.com>
2025-05-13 18:17:12 +0330

    add repo to customer and domain services

--- internal/customer/domain/customer.go
+++ internal/customer/domain/customer.go
@@ -1,5 +1,7 @@
 package domain
 
+import "errors"
+
 type (
 	CustomerID string
 	NationalID string
@@ -14,4 +16,28 @@ type Customer struct {
 	Address     string
 	RegionID    RegionID
 	CreatedAt   string
+}
+
+func (c *Customer) UpdatePhoneNumber(newPhoneNumber string) error {
+	if newPhoneNumber == "" {
+		return errors.New("phone number cannot be empty")
+	}
+	c.PhoneNumber = newPhoneNumber
+	return nil
+}
+
+func (c *Customer) UpdateAddress(newAddress string) error {
+	if newAddress == "" {
+		return errors.New("address cannot be empty")
+	}
+	c.Address = newAddress
+	return nil
+}
+
+func (c *Customer) UpdateRegion(newRegionID RegionID) error {
+	if newRegionID == "" {
+		return errors.New("region ID cannot be empty")
+	}
+	c.RegionID = newRegionID
+	return nil
 }
---
+++ internal/customer/domain/repository.go
@@ -0,0 +1,10 @@
+package domain
+
+import "context"
+
+type CustomerRepository interface {
+	FindByID(ctx context.Context, id CustomerID) (*Customer, error)
+	FindByNationalID(ctx context.Context, nID NationalID) (*Customer, error)
+	Save(ctx context.Context, customer *Customer) error
+	Delete(ctx context.Context, id CustomerID) error
+}
