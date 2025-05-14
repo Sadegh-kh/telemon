0d031506ffc9903b6fb2c8876e4b38a581118e81
Sadegh <Sadeghkhoshbayan1@gmail.com>
2025-05-14 19:49:31 +0330

    init create customer service

--- internal/customer/application/command.go
+++ internal/customer/application/command.go
@@ -16,6 +16,14 @@ type UpdateAddressCommand struct {
 	Address    string
 }
 
+type CreateCustomerCommand struct {
+	Name        string
+	PhoneNumber string
+	NationalID  domain.NationalID
+	Address     string
+	RegionID    domain.RegionID
+}
+
 func (s *CustomerService) UpdatePhoneNumber(ctx context.Context, cmd UpdatePhoneNumberCommand) error {
 	customer, err := s.repo.FindByID(ctx, cmd.CustomerID)
 	if err != nil {
@@ -36,4 +44,20 @@ func (s *CustomerService) UpdateAddress(ctx context.Context, cmd UpdateAddressCo
 	customer.UpdateAddress(cmd.Address)
 
 	return s.repo.Save(ctx, customer)
+}
+
+func (s *CustomerService) CreateCustomer(ctx context.Context, cmd CreateCustomerCommand) error {
+	customer, err := domain.NewCustomer(
+		domain.CustomerID(s.idGenerator.GenerateID()),
+		cmd.Name,
+		cmd.Address,
+		cmd.PhoneNumber,
+		cmd.NationalID,
+		cmd.RegionID,
+	)
+	if err != nil {
+		return err
+	}
+
+	return s.repo.Create(ctx, customer)
 }
